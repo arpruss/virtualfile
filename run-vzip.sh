4bfd0c59a63708ea158deb6352c003f5b9f62aa5
arpruss <arpruss@gmail.com>
2023-10-07 21:20:23 -0500

    clean

--- run-vzip.sh
+++ run-vzip.sh
@@ -1,13 +1,17 @@
 #mkdir mnt
-fusermount -u mnt
-python3 memory.py mnt $1.vzip &
+#fusermount -u mnt
+echo mounting
+python3 memory.py mnt ../$1.vzip &
 pid=$!
+echo waiting
 until [ -f mnt/$1.zip ]
 do
-    ls mnt
     sleep 1
 done
+echo ready
 ln -s mnt/$1.zip .
-c:/gog/mame/mame -rompath . $1
+#c:/gog/mame/mame -rompath . $1
+/home/pi/scripts/retroarch.sh "$1.zip" fbneo neogeo
 kill $pid
 fusermount -u mnt
+
