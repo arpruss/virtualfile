381374b21c08b9416082b9f41b24aa883b24fb05
arpruss <arpruss@gmail.com>
2023-10-10 22:29:48 -0500

    foo

--- run-vzip.sh
+++ run-vzip.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 #mkdir mnt
 #fusermount -u mnt
 echo mounting
-python3 memory.py mnt ../$1.vzip &
+python3 memory.py mnt "$1.vzip" &
 pid=$!
 echo waiting
 until [ -f mnt/$1.zip ]
@@ -12,8 +13,14 @@ echo ready
 ln -s mnt/$1.zip .
 #cp mnt/$1.zip .
 unzip -lv $1.zip
-#c:/gog/mame/mame -rompath . $1
+#mame -rompath . $1
+d=`pwd`
+if [[ $d == *mame-* ]] ; then
+/home/pi/scripts/retroarch.sh "$1.zip" mame2003 mame-libretro
+else
 /home/pi/scripts/retroarch.sh "$1.zip" fbneo neogeo
+fi
+#/home/pi/scripts/retroarch-mame.sh "$1.zip"
 kill $pid
 fusermount -u mnt
 
