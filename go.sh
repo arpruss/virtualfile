952ace0c176f969229883dea8430602320313e17
arpruss <arpruss@gmail.com>
2023-10-26 15:20:52 -0500

    foo

--- go.sh
+++ go.sh
@@ -5,5 +5,6 @@ until [ -f mem/*.zip ]
 do
     sleep 0.5
 done
-~/win/Downloads/mame/vmame64 -mouse -rompath mem mem/*
+#~/win/Downloads/mame/vmame64 -mouse -rompath mem mem/*
+~/win/Downloads/mame2003/mame -mouse -rompath mem mem/*
 kill $pid
