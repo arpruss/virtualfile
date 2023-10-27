4672c0ad79834d502ae5f6db5d315a3b94a78bd4
arpruss <arpruss@gmail.com>
2023-10-26 21:02:29 -0500

    rb

--- pad.py
+++ pad.py
@@ -2,17 +2,16 @@ import sys
 import ast
 
 with open(sys.argv[1],"rb") as fin:
-    offset = ast.literal_eval(sys.argv[2])
-    length = ast.literal_eval(sys.argv[3])
-    padBefore = ast.literal_eval(sys.argv[4])
-    padAfter = ast.literal_eval(sys.argv[5])
-    if len(sys.argv) > 6:
+    while len(sys.argv) > 6:
+        offset = ast.literal_eval(sys.argv[2])
+        length = ast.literal_eval(sys.argv[3])
+        padBefore = ast.literal_eval(sys.argv[4])
+        padAfter = ast.literal_eval(sys.argv[5])
         padValue = ast.literal_eval(sys.argv[6])
-    else:
-        padValue = 0
-    if padBefore:
-        sys.stdout.buffer.write(bytearray((padValue,)*padBefore))
-    fin.seek(offset)
-    sys.stdout.buffer.write(fin.read(length))
-    if padAfter:
-        sys.stdout.buffer.write(bytearray((padValue,)*padAfter))
+        if padBefore:
+            sys.stdout.buffer.write(bytearray((padValue,)*padBefore))
+        fin.seek(offset)
+        sys.stdout.buffer.write(fin.read(length))
+        if padAfter:
+            sys.stdout.buffer.write(bytearray((padValue,)*padAfter))
+        sys.argv = sys.argv[5:]
