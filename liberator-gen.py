c85c5f7f3d1605d59e238fdc90aa480a11da6f4d
arpruss <arpruss@gmail.com>
2023-11-02 20:12:18 -0500

    foo

--- liberator-gen.py
+++ liberator-gen.py
@@ -1,6 +1,8 @@
 import sys
 import math
 from PIL import Image
+from urllib.request import urlopen
+import re
 
 # in: 
 # 2 = white
@@ -17,6 +19,13 @@ from PIL import Image
 #colorMap = [15,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
 colorMap =  [15,15,1,2,3,4,5,6,7,8,9,10,11,12,13,15]
 
+def parseHex(url):
+    out = bytearray()
+    for line in urlopen(url).read().decode("ascii","ignore").split('\n'):
+        if re.match(r'^[0-9A-F][0-9A-F][0-9A-F][0-9A-F]=', line):
+            out += bytes(map(lambda x:int(x,16), line[5:].split(',')))
+    return out
+
 if sys.argv[1] == 'ltsmsbc' or sys.argv[1] == 'ltslsbc':
     for y in range(128):
         value = math.floor( 0.5 + 255 * math.sqrt( 1-((y-63.5)/64)**2 ) )
@@ -32,7 +41,8 @@ if sys.argv[1] == 'ltsmsbc' or sys.argv[1] == 'ltslsbc':
 elif sys.argv[1] == 'lgsmsbc' or sys.argv[1] == 'lgslsbc':
     for x in range(256):
         theta = (x-127.5)/127.5*math.pi/2
-        value = math.floor( 0.5 + (math.sin(theta)+1)/2. * 200 )
+        #value = math.floor( 0.25 + 3.5 + (math.sin(theta)+1)/2. * 196.4)
+        value = math.floor( 0.5 + (math.sin(theta)+1)/2. * 200)
         if value >= 200:
             value = 255
         elif value < 0:
@@ -44,13 +54,13 @@ elif sys.argv[1] == 'lgsmsbc' or sys.argv[1] == 'lgslsbc':
         else:
             sys.stdout.buffer.write(bytes((value&0xF,)))
 elif sys.argv[1] == 'ltsmsb':
-    sys.stdout.buffer.write(bytearray(ltsmsb))
+    sys.stdout.buffer.write(parseHex('https://github.com/historicalsource/liberator/raw/main/LTSMSB.ROM'))
 elif sys.argv[1] == 'ltslsb':
-    sys.stdout.buffer.write(bytearray(ltslsb))
+    sys.stdout.buffer.write(parseHex('https://github.com/historicalsource/liberator/raw/main/LTSLSB.ROM'))
 elif sys.argv[1] == 'lgsmsb':
-    sys.stdout.buffer.write(bytearray(lgsmsb))
+    sys.stdout.buffer.write(parseHex('https://github.com/historicalsource/liberator/raw/main/LGSMSB.ROM'))
 elif sys.argv[1] == 'lgslsb':
-    sys.stdout.buffer.write(bytearray(lgslsb))
+    sys.stdout.buffer.write(parseHex('https://github.com/historicalsource/liberator/raw/main/LGSLSB.ROM'))
 elif sys.argv[1] == 'image':
     image = Image.open("FOCAL_Emulator/Liberator.bmp")
     height = 256
@@ -72,7 +82,8 @@ elif sys.argv[1] == 'image':
             while len(segments)<32:
                 prev = 0
                 for i in range(len(segments)):
-                    if segments[i][1]-prev > 15:
+                    l = segments[i][1]-prev 
+                    if l > 16:
                         color = segments[i][0]
                         x = segments[i][1]
                         segments = segments[:i] + [(color,(prev+x)//2),(color,x)] + segments[i+1:]
